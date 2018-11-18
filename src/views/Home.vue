1628d3b2cbb9b4688af1a87b46428c3e75da002b
Timdev0 <quentin.peltier.pro@gmail.com>
2018-11-18 11:01:25 +0100

    add 1 photo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,9 +15,9 @@
             </div>
           </div>
           <!-- Slide Two - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+          <div class="carousel-item" style="background-image: url('/assets/4.jpg')">
             <div class="carousel-caption d-none d-md-block">
-              <p class="main-slide">Deuxième Slide</p>
+              <p class="main-slide">Portes</p>
             </div>
           </div>
           <!-- Slide Three - Set the background image for this slide in the line below -->
