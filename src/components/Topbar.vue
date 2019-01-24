4e05a4a18f38d4c2bec8544235c43c839172913d
Timdev0 <quentin.peltier.pro@gmail.com>
2019-01-24 11:05:36 +0100

    update topbar

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,39 +1,39 @@
 <template>
-    <div id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <router-link class="navbar-brand" to="/"><h2 class="h2-topbar">Ferronnerie Peltier</h2></router-link>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span>Menu </span><span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Accueil</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/galerie">Galerie</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/contact">Contact</router-link>
-            </li>
-          </ul>
-        </div>
-
-        </div>
+  <div id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">
+        <h2 class="h2-topbar">Ferronnerie Peltier</h2>
+      </router-link>
+      <button
+        class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span>Menu</span>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/galerie">Galerie</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/contact">Contact</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
-  .h2-topbar{
-    display: inline-block;
-    padding-top: 0.3125rem;
-    padding-bottom: 0.3125rem;
-    margin-right: 1rem;
-    font-size: 1.25rem;
-    line-height: inherit;
-    white-space: nowrap;
-    margin: 0;
-    padding: 0;
-  }
+.h2-topbar {
+  display: inline-block;
+  padding-top: 0.3125rem;
+  padding-bottom: 0.3125rem;
+  margin-right: 1rem;
+  font-size: 1.25rem;
+  line-height: inherit;
+  white-space: nowrap;
+  margin: 0;
+  padding: 0;
+}
 </style>
 
