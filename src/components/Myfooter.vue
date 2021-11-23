ba46797b2c444a43d430caad5f4d696b6150d66a
Timdev0 <quentin.peltier.pro@gmail.com>
2021-11-23 16:05:35 +0100

    update copyright

--- src/components/Myfooter.vue
+++ src/components/Myfooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="footer" class="py-4 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Ferronnerie PELTIER 2019</p>
+        <p class="m-0 text-center text-white">Copyright &copy; Ferronnerie PELTIER 2022</p>
       </div>
     </div>    
 </template>
